cd2f84d2ac5b816d708ff893a0ba739a369bf239
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-10-10 16:34:33 -0700

    Option for template path, working.

--- web/project/settings.py
+++ web/project/settings.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],  # CUSTOM: Added when adding users model app
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
