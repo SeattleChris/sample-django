681f20e0ebe0f29a408be67a344eb83cd65f868f
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-10-10 17:11:47 -0700

    Views example includes functional and class based.

--- web/project/views.py
+++ web/project/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def home_view(request):
     return render(request, 'generic/home.html')
+
+
+class NamedView(TemplateView):
+    template_name = "generic/base.html"
+    extra_context = {'css_sheets': ['css/home.css'], }
