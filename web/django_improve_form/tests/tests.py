65a0d50aad07a074d3db6b97f0432f50b0d3145c
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-10-18 04:27:46 -0700

    Added test helper into a test directory.

---
+++ web/django_improve_form/tests/tests.py
@@ -0,0 +1,10 @@
+# from django.test import TestCase  # , TransactionTestCase, Client, RequestFactory,
+# from django.conf import settings
+# from django.core.exceptions import ObjectDoesNotExist
+# from django.db.models import Q, Max, Subquery
+# from django.urls import reverse
+
+# from unittest import skip
+# from datetime import date, time, timedelta, datetime as dt
+
+# Create your tests here.
