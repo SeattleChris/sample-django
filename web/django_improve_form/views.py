3f91404baeb80d5dd86eced603e49f2e22c6773f
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-10-18 03:18:11 -0700

    Register views can set form_as_type to setup a desired form.as_<type>, or use typical setup. Translatable text used for default_context, which can be overriden by extra_context.

--- web/django_improve_form/views.py
+++ web/django_improve_form/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
+from django.utils.translation import gettext as _
 # from django.contrib.auth import get_user_model
 from django_registration.backends.one_step.views import RegistrationView as RegistrationViewOneStep
 from django_registration.backends.activation.views import RegistrationView as RegistrationViewTwoStep
@@ -14,16 +15,25 @@ from pprint import pprint  # TODO: Remove after debug
 @method_decorator(csrf_protect, name='dispatch')
 class RegisterSimpleFlowView(RegistrationViewOneStep):
     form_class = RegisterUserForm
+    form_as_type = None
     success_url = reverse_lazy('profile_page')
     # template_name = 'improve_form/signup.html'
     template_name = 'django_registration/registration_form.html'
-    extra_context = {
-        'improve_form_title': 'New User Sign Up',
+    default_context = {
+        'improve_form_title': _('New User Sign Up'),
         'improve_form_container_class': 'registration-form-container',
-        'form_heading': 'Sign Up Now!',
-        'form_preamble': 'read this before the form starts',
-        'form_button_text': 'Sign Up',
+        'form_heading': _('Sign Up Now!'),
+        'form_preamble': '',
+        'form_button_text': _('Sign Up'),
         }
+
+    def get_context_data(self, **kwargs):
+        kwargs['form_as_type'] = getattr(self, 'form_as_type', None)
+        extra_context = getattr(self, 'default_context', {})
+        extra_context.update(getattr(self, 'extra_context', {}) or {})
+        self.extra_context = extra_context  # Allowing user defined context to override default_context
+        context = super().get_context_data(**kwargs)
+        return context
 
     def register(self, form):
         print("===================== RegisterSimpleFlowView.register ============================")
@@ -36,16 +46,25 @@ class RegisterSimpleFlowView(RegistrationViewOneStep):
 @method_decorator(csrf_protect, name='dispatch')
 class RegisterActivateFlowView(RegistrationViewTwoStep):
     form_class = RegisterUserForm
+    form_as_type = None
     success_url = reverse_lazy('profile_page')
     # template_name = 'improve_form/signup.html'
     template_name = 'django_registration/registration_form.html'
-    extra_context = {
-        'improve_form_title': 'New User Sign Up',
+    default_context = {
+        'improve_form_title': _('New User Sign Up'),
         'improve_form_container_class': 'registration-form-container',
-        'form_heading': 'Sign Up Now!',
-        'form_preamble': 'read this before the form starts',
-        'form_button_text': 'Sign Up',
+        'form_heading': _('Sign Up Now!'),
+        'form_preamble': '',
+        'form_button_text': _('Sign Up'),
         }
+
+    def get_context_data(self, **kwargs):
+        kwargs['form_as_type'] = getattr(self, 'form_as_type', None)
+        extra_context = getattr(self, 'default_context', {})
+        extra_context.update(getattr(self, 'extra_context', {}) or {})
+        self.extra_context = extra_context  # Allowing user defined context to override default_context
+        context = super().get_context_data(**kwargs)
+        return context
 
     def register(self, form):
         print("===================== RegisterActivateFlowView.register ============================")
