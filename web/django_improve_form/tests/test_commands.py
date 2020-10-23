82fbe5dc2b4677e5947126cbc7f4f7f1d31c204b
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-10-23 00:05:31 -0700

    Tests - urllist command: extened tests for get_sub_rules conditions.

--- web/django_improve_form/tests/test_commands.py
+++ web/django_improve_form/tests/test_commands.py
@@ -17,7 +17,7 @@ class UrllistTests(TestCase):
     base_opts.update({'long': True, 'sort': urllist.initial_sort, 'sub_cols': urllist.initial_sub_cols, })
     # 'long' and 'data' are False by default, but most of our tests use True for clarity sake.
 
-    def test_get_col_names(self):
+    def test_get_col_names_by_priority(self):
         """Confirm it processes correctly when the 'only' parameter has an integer value. """
         priorities = urllist.column_priority
         all_cols = urllist.all_columns
@@ -29,10 +29,50 @@ class UrllistTests(TestCase):
             actual = self.com.get_col_names(opts)
             self.assertListEqual(expected, actual)
 
-    def test_sub_rules_when_not_long(self):
-        """The sub_rules are expected to include the defaults when long is not set to True. """
+    def test_sub_rules_default(self):
+        """The sub_rules are expected to include the defaults for default settings (long=False). """
         expected = [(*rule, urllist.initial_sub_cols) for rule in urllist.initial_sub_rules]
         opts = self.base_opts.copy()
         opts['long'] = False
+        actual = self.com.get_sub_rules(opts)
+        self.assertListEqual(expected, actual)
+
+    def test_sub_rules_when_long(self):
+        """The sub_rules are expected to be empty when 'long' is True and there are no added sub_rules. """
+        expected = []
+        opts = self.base_opts.copy()
+        actual = self.com.get_sub_rules(opts)
+        self.assertListEqual(expected, actual)
+
+    def test_add_sub_rules_default(self):
+        """The sub_rules are expected to include the defaults and added rules, on default columns. """
+        expected = [(*rule, urllist.initial_sub_cols) for rule in urllist.initial_sub_rules]
+        new_rules = [('dog', 'woof'), ('cat', 'meow')]
+        expected += [(*rule, urllist.initial_sub_cols) for rule in new_rules]
+        opts = self.base_opts.copy()
+        opts['long'] = False
+        opts['add'] = new_rules
+        actual = self.com.get_sub_rules(opts)
+        self.assertListEqual(expected, actual)
+
+    def test_add_sub_rules_default_with_cols(self):
+        """The sub_rules are expected to include the defaults on their columns, added on defined columns. """
+        expected = [(*rule, urllist.initial_sub_cols) for rule in urllist.initial_sub_rules]
+        new_rules = [('dog', 'woof'), ('cat', 'meow')]
+        cols = ['pattern', 'args']
+        expected += [(*rule, cols) for rule in new_rules]
+        opts = self.base_opts.copy()
+        opts['long'] = False
+        opts['add'] = new_rules
+        opts['cols'] = cols
+        actual = self.com.get_sub_rules(opts)
+        self.assertListEqual(expected, actual)
+
+    def test_add_sub_rules_when_long_no_cols(self):
+        """The sub_rules are expected to only include the added rules, on the default columns. """
+        new_rules = [('dog', 'woof'), ('cat', 'meow')]
+        expected = [(*rule, urllist.initial_sub_cols) for rule in new_rules]
+        opts = self.base_opts.copy()
+        opts['add'] = new_rules
         actual = self.com.get_sub_rules(opts)
         self.assertListEqual(expected, actual)
